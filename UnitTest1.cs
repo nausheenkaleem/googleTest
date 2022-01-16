39d8aa66362f0442a49c8d09963181eec95795a0
nausheenkaleem <50286671+nausheenkaleem@users.noreply.github.com>
2022-01-17 01:06:57 +0500

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace googleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestSearchStreetFighterVThenVerifyStreetFighterVIsDisplayed()
+        {
+            int waitingTime = 2000;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+            By googleResultText = By.XPath(".//h2//span[text()='Street Fighter V']"); By googleIAgreeButton = By.Id("L2AGLb");
+
+            IWebDriver webDriver = new ChromeDriver(); Thread.Sleep(waitingTime); webDriver.Navigate().GoToUrl("https://www.google.co.uk");
+
+
+            //webDriver.FindElement(googleIAgreeButton).Click(); webDriver.Manage().Window.Maximize(); Thread.Sleep(waitingTime);
+            webDriver.FindElement(googleSearchBar).SendKeys("Street Fighter V"); Thread.Sleep(waitingTime); webDriver.FindElement(googleSearchButton).Click(); Thread.Sleep(waitingTime);
+
+            var actualResultText = webDriver.FindElement(googleResultText); Assert.IsTrue(actualResultText.Text.Equals("Street Fighter V"));
+            webDriver.Quit();
+        }
+    }
+}
